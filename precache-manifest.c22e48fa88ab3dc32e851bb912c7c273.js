de79164edf670fd379ed7b7687292b3c3e27b3bb
ShakMR <borja.arias.upc@gmail.com>
2021-12-02 16:00:46 +0100

    Updates

---
+++ precache-manifest.c22e48fa88ab3dc32e851bb912c7c273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475aedadea5c4fa007c062024b768fd0",
+    "url": "/signature-templater/index.html"
+  },
+  {
+    "revision": "615f36473b683f3b130f",
+    "url": "/signature-templater/static/css/main.d583a2a0.chunk.css"
+  },
+  {
+    "revision": "3ee26bc6099b2cd5c57d",
+    "url": "/signature-templater/static/js/2.80678444.chunk.js"
+  },
+  {
+    "revision": "615f36473b683f3b130f",
+    "url": "/signature-templater/static/js/main.0c69b86d.chunk.js"
+  },
+  {
+    "revision": "7abe1fd8effb77cbda7c",
+    "url": "/signature-templater/static/js/runtime-main.de765648.js"
+  }
+]);
